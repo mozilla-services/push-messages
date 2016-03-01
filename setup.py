5c048d32cc4fd2ca97d38a473700fc85f591513d
Ben Bangert <ben@groovie.org>
2016-03-01 10:25:09 -0800

    chore: add initial project files and project base

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGELOG.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='push_messages',
+      version='0.1',
+      description='push_messages',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="push_messages",
+      entry_points="""\
+      [paste.app_factory]
+      main = push_messages:main
+      """,
+      )
